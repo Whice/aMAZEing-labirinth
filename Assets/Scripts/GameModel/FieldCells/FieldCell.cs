995fcd0dd7622c10571d650f99af571b043603df
A.Baranov <a.baranov@overmindgroup.com>
2022-03-15 10:22:45 +0300

    Add in model abstract class for cells.

---
+++ Assets/Scripts/GameModel/FieldCells/FieldCell.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.GameModel.FieldCells
+{
+    /// <summary>
+    ///  Направление прохода.
+    /// </summary>
+    public enum PassageDirection
+    {
+        /// <summary>
+        /// Проход отсутствует.
+        /// </summary>
+        none,
+        /// <summary>
+        /// Вверх.
+        /// </summary>
+        up,
+        /// <summary>
+        /// Влево.
+        /// </summary>
+        left,
+        /// <summary>
+        /// Вниз.
+        /// </summary>
+        down,
+        /// <summary>
+        /// Вправо.
+        /// </summary>
+        right
+    }
+    public abstract class FieldCell
+    {
+        /// <summary>
+        /// Проходы.
+        /// </summary>
+        protected List<PassageDirection> directions = null;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="directions">Проходы.</param>
+        public FieldCell(List<PassageDirection> directions)
+        {
+            this.directions = directions;
+        }
+
+
+    }
+}
