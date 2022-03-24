ef2c3bd5f708bff6a5170fb5a2c421cb6d81dbb7
A.Baranov <a.baranov@overmindgroup.com>
2022-03-24 13:08:29 +0300

    Добавлен класс карты.

---
+++ Assets/Scripts/GameModel/CardDeck/Card.cs
@@ -0,0 +1,47 @@
+﻿using Assets.Scripts.GameModel.PlayingField.Treasures;
+using System;
+
+namespace Assets.Scripts.GameModel.CardDeck
+{
+    /// <summary>
+    /// Катра для игры.
+    /// </summary>
+    public class Card
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="treasure">Сокровище для этой карты.</param>
+        /// <param name="isOpen">Открыта ли карта, по умолчанию нет.</param>
+        public Card(TreasureAndStartPointsType treasure, Boolean isOpen = false)
+        {
+            this.treasure = treasure;
+            this.isOpenField = isOpen;
+        }
+
+        /// <summary>
+        /// Сокровище, с которым связана эта карта.
+        /// </summary>
+        public readonly TreasureAndStartPointsType treasure = TreasureAndStartPointsType.unknown;
+        /// <summary>
+        /// Раскрыта ли карта.
+        /// <br/>Если карта та раскрыта, значит она лежит "рубашкой" вниз.
+        /// </summary>
+        private Boolean isOpenField = false;
+        /// <summary>
+        /// Раскрыта ли карта.
+        /// <br/>Если карта та раскрыта, значит она лежит "рубашкой" вниз.
+        /// </summary>
+        public Boolean isOpen
+        {
+            get => this.isOpenField;
+        }
+        /// <summary>
+        /// Перевернуть карту.
+        /// </summary>
+        public void FlipCard()
+        {
+            this.isOpenField = !this.isOpenField;
+        }
+    }
+}
--- Assets/Scripts/GameModel/GamePlayer.cs
+++ Assets/Scripts/GameModel/GamePlayer.cs
@@ -8,18 +8,37 @@ namespace Assets.Scripts.GameModel
     /// </summary>
     public class GamePlayer
     {
+        /// <summary>
+        /// Имя или прозвище игрока.
+        /// </summary>
         public readonly String name;
+        /// <summary>
+        /// Цвет игрока.
+        /// </summary>
         public readonly Color color;
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name">Имя или прозвище игрока.</param>
+        /// <param name="color">Цвет игрока.</param>
         public GamePlayer(String name, Color color)
         {
             this.name = name;
             this.color = color;
         }
 
-
+        /// <summary>
+        /// Местоположение на поле по оси X.
+        /// </summary>
         public Int32 positionX;
+        /// <summary>
+        /// Местоположение на поле по оси Y.
+        /// </summary>
         public Int32 positionY;
+        /// <summary>
+        /// Местоположение на поле.
+        /// </summary>
         public Point position
         {
             get
