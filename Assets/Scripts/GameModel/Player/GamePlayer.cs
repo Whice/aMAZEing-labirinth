ddcf1c38dfec24b87cbd67d81495438fd25e1127
A.Baranov <a.baranov@overmindgroup.com>
2022-03-25 12:30:10 +0300

    Изменения в папках для карт и игрока.

--- Assets/Scripts/GameModel/Player/GamePlayer.cs
+++ Assets/Scripts/GameModel/Player/GamePlayer.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 
-namespace Assets.Scripts.GameModel
+namespace Assets.Scripts.GameModel.Player
 {
     /// <summary>
     /// Игрок в партии.
