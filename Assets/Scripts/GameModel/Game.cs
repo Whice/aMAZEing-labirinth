42e32975d925364bf1ee1a9f3bbe5390bc5d9df1
A.Baranov <a.baranov@overmindgroup.com>
2022-03-29 15:03:39 +0300

    В класс игры добавлены конструктор, метод начала, данные о фазе и поле.

--- Assets/Scripts/GameModel/Game.cs
+++ Assets/Scripts/GameModel/Game.cs
@@ -1,4 +1,7 @@
 ﻿using Assets.Scripts.GameModel.Player;
+using Assets.Scripts.GameModel.PlayingField;
+using Assets.Scripts.GameModel.PlayingField.FieldCells;
+using Assets.Scripts.GameModel.TurnPhaseAndExtensions;
 using System;
 using System.Collections.Generic;
 
@@ -16,6 +19,27 @@ namespace Assets.Scripts.GameModel
         }
 
         /// <summary>
+        /// Выбрать начального игрока и вернуть его номер.
+        /// </summary>
+        /// <returns></returns>
+        private Int32 ChooseStartPlayerNumber()
+        {
+            Random random = new Random();
+            return random.Next(players.Length);
+        }
+        /// <summary>
+        /// Начать игру.
+        /// </summary>
+        public void Start()
+        {
+            this.currentPlayerNumber = ChooseStartPlayerNumber();
+            this.currentPhasePrivate = TurnPhase.movingCell;
+            this.fieldPrivate = new Field();
+        }
+
+        #region Данные игры.
+
+        /// <summary>
         /// Список игроков.
         /// </summary>
         private readonly GamePlayer[] players = null;
@@ -30,6 +54,45 @@ namespace Assets.Scripts.GameModel
         {
             get=>this.players[currentPlayerNumber];
         }
+        /// <summary>
+        /// Текущая фаза хода игрока.
+        /// </summary>
+        private TurnPhase currentPhasePrivate = TurnPhase.unknow;
+        /// <summary>
+        /// Текущая фаза хода игрока.
+        /// </summary>
+        public TurnPhase currentPhase
+        {
+            get => this.currentPhasePrivate;
+        }
+        /// <summary>
+        /// Игровое поле.
+        /// </summary>
+        private Field fieldPrivate = null;
+        /// <summary>
+        /// Игровое поле.
+        /// </summary>
+        public Field field
+        {
+            get => this.fieldPrivate;
+        }
+        /// <summary>
+        /// Свободная ячейка.
+        /// </summary>
+        public FieldCell freeCell
+        {
+            get => this.field.freeFieldCell;
+        }
 
+        #endregion Данные игры.
+
+        #region Действия.
+
+        public void SetFreeCellToField(Int32 x, Int32 y)
+        {
+            
+        }
+
+        #endregion Действия.
     }
 }
