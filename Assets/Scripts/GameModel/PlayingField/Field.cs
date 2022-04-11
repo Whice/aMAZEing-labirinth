02f7e666f9ae49a41f4317d2c7d1f264d8d6861f
A.Baranov <a.baranov@overmindgroup.com>
2022-04-11 16:08:32 +0300

    Добавлена проверка на выход за пределы поля при перемещении фигурки игрока.

--- Assets/Scripts/GameModel/PlayingField/Field.cs
+++ Assets/Scripts/GameModel/PlayingField/Field.cs
@@ -476,7 +476,16 @@ namespace Assets.Scripts.GameModel.PlayingField
         /// <returns></returns>
         public Boolean IsPossibleMove(Int32 startX,Int32 startY,Int32 endX,Int32 endY)
         {
+            //Проверить, что все координаты оказались внутрии поля.
+            Boolean isWithinField = startX>-1 && startY>-1 && endX>-1 && endY>-1;
+            isWithinField &= startX<Field.FIELD_SIZE && startY<Field.FIELD_SIZE && endX<Field.FIELD_SIZE && endY>Field.FIELD_SIZE;
+            if(!isWithinField)
+            {
+                return false;
+            }
+
             //ToDo тут должен быть алгоритм рассчета пути.
+
             return true;
         }
     }
