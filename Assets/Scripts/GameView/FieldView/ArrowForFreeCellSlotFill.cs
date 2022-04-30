53410feebb0198b1e254c76b121b6cafcd8c58b6
A.Baranov <a.baranov@overmindgroup.com>
2022-04-30 19:41:15 +0300

    Добавлен слот для стрелочки-места для свободной ячейки.

---
+++ Assets/Scripts/GameView/FieldView/ArrowForFreeCellSlotFill.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace Assets.Scripts.GameView.FieldView
+{
+    /// <summary>
+    /// —лот стрелочки, куда будет помещена свободна€ €чейка.
+    /// <br/>–азмер и позици€ слота завис€т от соответствующих параметров €чейки €чейки.
+    /// </summary>
+    public class ArrowForFreeCellSlotFill : CellSlotView
+    {
+        /// <summary>
+        /// ќбъект стрелочки из провайдера дл€ слота.
+        /// </summary>
+        private GameObject arrowObject = null;
+    }
+}
--- Assets/Scripts/GameView/FieldView/FieldView.cs
+++ Assets/Scripts/GameView/FieldView/FieldView.cs
@@ -6,27 +6,64 @@ using UnityEngine;
 
 namespace Assets.Scripts.GameView.FieldView
 {
+    /// <summary>
+    /// Представление игрового поля.
+    /// </summary>
     public class FieldView: MonoBehaviour
     {
+        /// <summary>
+        /// Слот, куда будет помещеная ячейка.
+        /// </summary>
         [SerializeField]
         private CellSlotFill cellSlotPrefab = null;
+        /// <summary>
+        /// Размер слота ячейки.
+        /// </summary>
+        private Vector3 sizeCellSlot;
+        /// <summary>
+        /// Все слоты поля.
+        /// </summary>
         private CellSlotFill[,] slots = new CellSlotFill[Field.FIELD_SIZE, Field.FIELD_SIZE];
-        private void FillField()
+        /// <summary>
+        /// Добавить слоты для стрелочек - мест, куда будет помещена свободная ячейка.
+        /// </summary>
+        private void AddArrowSlotForFreeCell()
         {
-            Vector3 size = this.cellSlotPrefab.size;
+            Vector3 size = this.sizeCellSlot;
+            for (Int32 i=1;i<Field.FIELD_SIZE;i++)
+            {
+
+            }
+        }
+        /// <summary>
+        /// Заполнить игровое поле ячейками.
+        /// </summary>
+        private void FillFieldWithCell()
+        {
+            //Заполнение ячейками.
+            Vector3 size = this.sizeCellSlot;
             for (Int32 i = 0; i < Field.FIELD_SIZE; i++)
                 for (Int32 j = 0; j < Field.FIELD_SIZE; j++)
                 {
                     this.slots[i, j] = Instantiate(this.cellSlotPrefab);
                     this.slots[i, j].SetCellType((CellType)UnityEngine.Random.Range(1, 4));
-                    size = this.slots[i, j].size;
                     this.slots[i, j].position = new Vector3(i * (size.x + 1), slots[i, j].position.y, j * (size.z + 1));
                     this.slots[i, j].transform.parent = this.transform;
                 }
+
+
         }
         private void Awake()
         {
-            FillField();
+            //Задать слоту ячейку и потом считать из нее размер, т.к. размер слота зависит от размера ячейки.
+            {
+                CellSlotFill fillingSlot = Instantiate(this.cellSlotPrefab);
+                fillingSlot.SetCellType(CellType.line);
+                this.sizeCellSlot = fillingSlot.size;
+            }
+
+            FillFieldWithCell();
+            AddArrowSlotForFreeCell();
         }
     }
 }
