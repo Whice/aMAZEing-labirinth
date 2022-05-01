7bf14c48feb0eadf14f5f9068d0b2fb4a6339c23
A.Baranov <a.baranov@overmindgroup.com>
2022-05-01 18:43:42 +0300

    Реализация логгера.

--- Assets/Scripts/MonoBehaviourLogger.cs
+++ Assets/Scripts/MonoBehaviourLogger.cs
@@ -1,4 +1,6 @@
-﻿namespace UnityEngine
+﻿using System;
+
+namespace UnityEngine
 {
     /// <summary>
     /// Создан для упрощения выдачи сообщений в консоль,
@@ -6,5 +8,29 @@
     /// </summary>
     public class MonoBehaviourLogger : MonoBehaviour
     {
+        /// <summary>
+        /// Вывести сообщение об ошибке в консоль.
+        /// </summary>
+        /// <param name="message"></param>
+        protected void LogError(String message)
+        {
+            Debug.LogError(message);
+        }
+        /// <summary>
+        /// Вывести предупреждене в консоль.
+        /// </summary>
+        /// <param name="message"></param>
+        protected void LogWarning(String message)
+        {
+            Debug.LogWarning(message);
+        }
+        /// <summary>
+        /// Вывести сообщение в консоль.
+        /// </summary>
+        /// <param name="message"></param>
+        protected void LogInfo(String message)
+        {
+            Debug.Log(message);
+        }
     }
 }
