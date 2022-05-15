791a99eb53b43cf5e79ceb9827f939ed8903c640
A.Baranov <a.baranov@overmindgroup.com>
2022-05-15 18:50:44 +0300

    feat: начата полноценная работа с UI.

---
+++ Assets/UI/Camera/GameInterface/Cards/UICardWithTreasureSlot.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    /// <summary>
+    /// Слот для карты с сокровищем.
+    /// </summary>
+    public class UICardWithTreasureSlot : GameViewOriginScript
+    {
+        #region Внешние даные.
+
+        /// <summary>
+        /// Рубашка карты.
+        /// </summary>
+        [SerializeField]
+        private GameObject cardBack = null;
+        /// <summary>
+        /// Лицо карты.
+        /// </summary>
+        [SerializeField]
+        private GameObject cardFace = null;
+        /// <summary>
+        /// Слот картинки сокровища.
+        /// </summary>
+        [SerializeField]
+        private Image treasureSlot = null;
+
+        #endregion Внешние даные.
+
+        /// <summary>
+        /// Карта лежит рубашкой вверх.
+        /// По умолчанию true.
+        /// </summary>
+        private Boolean isCardBackUp = true;
+    }
+}
