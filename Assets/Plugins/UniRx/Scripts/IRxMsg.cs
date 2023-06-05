1e625c0e955c9d5442bf0361d8f94105ebef9395
Forni <and-not@mail.ru>
2023-06-05 11:56:35 +0300

    feat: добавление расширений для UniRx.

---
+++ Assets/Plugins/UniRx/Scripts/IRxMsg.cs
@@ -0,0 +1,7 @@
+﻿
+namespace SummonEra.RxEvents
+{
+    public interface IRxMsg
+    {
+    }
+}
---
+++ Assets/Plugins/UniRx/Scripts/RxEventExtension.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using UniRx;
+using DG.Tweening;
+
+namespace SummonEra.RxEvents
+{
+    public static class RxEventExtension
+    {
+        public static CompositeDisposable Subscribe<TEvent>(this CompositeDisposable disposables, Action<TEvent> msg) where TEvent : IRxMsg
+        {
+            MessageBroker.Default
+                .Receive<IRxMsg>()
+                .Where(x => x.GetType() == typeof(TEvent))
+                .Select(x => (TEvent)x)
+                .Subscribe(msg)
+                .AddTo(disposables);
+            return disposables;
+        }
+
+        public static void Publish(this IRxMsg msg)
+        {
+            MessageBroker.Default.Publish(msg);
+        }
+
+        public static void Publish(this IRxMsg msg, float delay)
+        {
+            float t = 0f;
+            DOTween.To(() => t, x => t = x, delay, delay).OnComplete(() =>
+            {
+                msg.Publish();
+            });
+        }
+    }
+}
